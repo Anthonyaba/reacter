e551189eb2d0550906831d75a13daef79552011f
Anthonyaba <37057416+Anthonyaba@users.noreply.github.com>
2018-03-31 14:26:25 +0800

    Add files via upload

---
+++ bar.js
@@ -0,0 +1,134 @@
+﻿import React from 'react';
+import {Component} from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui-icons/Menu';
+import AccountCircle from 'material-ui-icons/AccountCircle';
+import Switch from 'material-ui/Switch';
+import { FormControlLabel, FormGroup } from 'material-ui/Form';
+import Menu, { MenuItem } from 'material-ui/Menu';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+    height: 430,
+    zIndex: 1,
+    overflow: 'hidden',
+    position: 'relative',
+
+  },
+  flex: {
+    flex: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+class MenuAppBar extends Component {
+  state = {
+    auth: true,
+    anchorEl: null,
+  };
+
+  handleChange = (event, checked) => {
+    this.setState({ auth: checked });
+  };
+
+
+
+  handleMenu = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+
+  
+  render() {
+    const { classes } = this.props;
+    const { auth, anchorEl } = this.state;
+    const open = Boolean(anchorEl);
+
+    return (
+      <div className={classes.root}>
+        <FormGroup>
+        
+        
+          <FormControlLabel
+            control={
+              <Switch checked={auth} onChange={this.handleChange} aria-label="LoginSwitch" />  
+            }
+            label={auth ? '登出' : '登录'}
+          />
+           </FormGroup>
+        
+        
+        <AppBar position="1">  
+          <Toolbar>  
+          
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu"
+             onClick={this.handleMenu1} >
+              <MenuIcon />  
+
+            </IconButton>
+
+
+
+            <Typography variant="title" color="inherit" className={classes.flex}>
+              Welcome            
+            </Typography>
+
+
+            {auth && (                                     //应用栏右侧的登录标志
+              <div>
+                
+                <IconButton
+                  aria-owns={open ? 'menu-appbar' : null}  
+                  onClick={this.handleMenu}
+                  color="inherit"
+                >
+                  <AccountCircle />
+                </IconButton>  
+               
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}           //该程序段为打开应用栏右侧圆形按钮所示的菜单
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={open}
+                  onClose={this.handleClose}
+                >
+                  <MenuItem onClick={this.handleClose}>菜单</MenuItem>
+
+                  <MenuItem onClick={this.handleClose}>菜单项2</MenuItem>
+                </Menu>
+              
+              </div>
+            )}
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+MenuAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(MenuAppBar);
---
+++ drawer图案.js
@@ -0,0 +1,41 @@
+﻿
+import React from 'react';
+import { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
+import InboxIcon from 'material-ui-icons/MoveToInbox';
+
+
+import SendIcon from 'material-ui-icons/Send';
+import MailIcon from 'material-ui-icons/Mail';
+
+
+
+export const fuction1 = (
+  <div>
+    <ListItem button>
+      <ListItemIcon>
+        <InboxIcon />
+      </ListItemIcon>
+      <ListItemText primary="功能1" />
+    </ListItem>
+
+    <ListItem button>
+      <ListItemIcon>
+        <SendIcon />
+      </ListItemIcon>
+      <ListItemText primary="功能2" />
+    </ListItem>
+
+  </div>
+);
+
+export const fuction2 = (
+  <div>
+    <ListItem button>
+      <ListItemIcon>
+        <MailIcon />
+      </ListItemIcon>
+      <ListItemText primary="功能3" />
+    </ListItem>
+ 
+  </div>
+);
