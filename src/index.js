24403a6f10d8374c39f26674e0742162e93ead70
Anthonyaba <37057416+Anthonyaba@users.noreply.github.com>
2018-03-31 14:30:50 +0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import MenuAppBar from './bar';
+import PersistentDrawer from './drawer';
+const App=(props,context)=>{
+
+  return (
+    <div>
+    <PersistentDrawer>
+    </PersistentDrawer>
+  </div>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
